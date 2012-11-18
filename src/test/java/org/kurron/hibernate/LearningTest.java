93196b03195248eed453e3b6564a9193c1d834bb
Ronald D. Kurr <kurr@kurron.org>
2012-11-18 16:09:19 -0500

    Initial unit test.

--- src/test/java/org/kurron/hibernate/LearningTest.java
+++ src/test/java/org/kurron/hibernate/LearningTest.java
@@ -1,11 +1,15 @@
 package org.kurron.hibernate;
 
+import org.junit.Test;
+
 /**
- * Created with IntelliJ IDEA.
- * User: ron
- * Date: 11/18/12
- * Time: 4:02 PM
- * To change this template use File | Settings | File Templates.
+ * Learning test for Hibernate associations.
  */
 public class LearningTest {
+
+    @Test
+    public void given_when_then() throws Exception
+    {
+        System.err.println("Test called.");
+    }
 }

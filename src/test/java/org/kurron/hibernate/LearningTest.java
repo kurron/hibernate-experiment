37cd117d8340fc83282f4587dde1c7fdd8e39977
Ronald D. Kurr <kurr@kurron.org>
2012-11-18 16:04:27 -0500

    Initial project set up.

---
+++ src/test/java/org/kurron/hibernate/LearningTest.java
@@ -0,0 +1,11 @@
+package org.kurron.hibernate;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ron
+ * Date: 11/18/12
+ * Time: 4:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LearningTest {
+}

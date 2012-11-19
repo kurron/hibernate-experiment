a8d065e4ef934da53e5f12db001d52f013fc3fb3
Ronald D. Kurr <kurr@kurron.org>
2012-11-18 21:29:29 -0500

    Configured logging a bit.

--- src/test/java/org/kurron/hibernate/LearningTest.java
+++ src/test/java/org/kurron/hibernate/LearningTest.java
@@ -1,18 +1,28 @@
 package org.kurron.hibernate;
 
+import org.hibernate.SessionFactory;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 /**
  * Learning test for Hibernate associations.
  */
 @ContextConfiguration
-public class LearningTest extends AbstractTransactionalJUnit4SpringContextTests {
+public class LearningTest extends AbstractTransactionalJUnit4SpringContextTests
+{
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @Test
     public void given_when_then() throws Exception
     {
         System.err.println("Test called.");
+        assertThat( sessionFactory, is( notNullValue() ) );
     }
 }
